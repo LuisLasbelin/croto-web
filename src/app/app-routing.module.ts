7ed8ac44a81bd61c3ca56153fe3c6d770a076845
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-09-15 16:25:04 +0200

    routing again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { BlogEntryEditorComponent } from './admin/blog-entry-editor/blog-entry-editor.component';
-import { BlogListComponent } from './blog-list/blog-list.component';
 import { BlogViewerComponent } from './blog-viewer/blog-viewer.component';
-import { CharacterGalleryComponent } from './character-gallery/character-gallery.component';
 import { LandingComponent } from './landing/landing.component';
-import { MapaMundiComponent } from './mapa-mundi/mapa-mundi.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
-  { path: 'blog-list', component: BlogListComponent },
   { path: 'landing', component: LandingComponent },
   { path: 'blog-viewer/:id', component: BlogViewerComponent },
   { path: 'blog-editor', component: BlogEntryEditorComponent },
-  { path: 'character-gallery', component: CharacterGalleryComponent },
-  { path: 'mapamundi', component: MapaMundiComponent },
   { path: 'admin', component: AdminPanelComponent}
 ];
 
