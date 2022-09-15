90e1318e0e844502abc9c0b3d11f0fe2c63da95c
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-09-15 16:30:25 +0200

    redirectTo app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { BlogEntryEditorComponent } from './admin/blog-entry-editor/blog-entry-editor.component';
-import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 import { BlogViewerComponent } from './blog-viewer/blog-viewer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
-  { path: 'landing', component: AppModule },
+  { path: 'landing', component: AppComponent },
   { path: 'blog-viewer/:id', component: BlogViewerComponent },
   { path: 'blog-editor', component: BlogEntryEditorComponent },
   { path: 'admin', component: AdminPanelComponent},
-  { path: '**', component: AppModule }
+  { path: '**', component: AppComponent }
 ];
 
 @NgModule({
