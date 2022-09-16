7cfd79c514fed19dc3fd7aea21e976c671d8d92b
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-09-16 12:25:55 +0200

    mapa y list wrap

--- src/app/mapa-mundi/mapa-mundi.component.ts
+++ src/app/mapa-mundi/mapa-mundi.component.ts
@@ -10,6 +10,15 @@ export class MapaMundiComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+    img.onload = () => {
+      ctx?.drawImage(img, 0, 0, canvas.width, img.height*(canvas.width/img.width));
+    };
+    img.src = '../../assets/Mapa/Mapa general.jpg';
   }
 
 }
