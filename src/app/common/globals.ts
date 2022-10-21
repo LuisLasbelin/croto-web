f8c8f214fb9916de981752037ebe9011e2c717cf
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-10-21 12:13:34 +0200

    loading global and removed animation procedures in landing

---
+++ src/app/common/globals.ts
@@ -0,0 +1,11 @@
+export class GlobalVariables {
+    private static loadingPage: boolean = true;
+
+    public static getLoadingStatus(): boolean {
+        return this.loadingPage;
+    }
+
+    public static setLoadingStatus(state: boolean ) {
+        this.loadingPage = state;
+    }
+}
